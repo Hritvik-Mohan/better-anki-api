5c2c2fd7be7f99d51ddd03ab87af76b37c738f3e
Hritvik-Mohan <hritvik5005@gmail.com>
2025-01-06 01:16:24 +0530

    Add progress controller functions scaffold

---
+++ src/controllers/progressController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const getUserProgressForDeck = async (req: Request, res: Response): Promise<void> => {
+}
+
+export const getUserProgressForAllDecks = async (req: Request, res: Response): Promise<void> => {
+}
+
+export const updateUserProgressForCard = async (req: Request, res: Response): Promise<void> => {
+}
+
+export const resetUserProgressForDeck = async (req: Request, res: Response): Promise<void> => {
+}
