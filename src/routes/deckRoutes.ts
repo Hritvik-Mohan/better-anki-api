360abe7c7e934149572de9fb5be224258fa37cfa
Hritvik-Mohan <hritvik5005@gmail.com>
2025-01-05 23:43:17 +0530

    Add deck routes

---
+++ src/routes/deckRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { createDeck, deleteDeck, getAllDecks, getDeckById, updateDeck } from "../controllers/deckController";
+
+const router = express.Router();
+
+router.get("/deck", getAllDecks);
+router.post("/deck", createDeck);
+router.get("/deck/{deckId}", getDeckById);
+router.put("/deck/{deckId}", updateDeck);
+router.delete("/deck/{deckId}", deleteDeck);
+
+export default router;
