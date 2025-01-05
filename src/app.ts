c8fe8817a07db41e2be751c24284228d1cc19914
Hritvik-Mohan <hritvik5005@gmail.com>
2025-01-06 00:02:12 +0530

    Update app.ts: add correct route modules

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
+import deckRoutes from "./routes/deckRoutes";
+import cardRoutes from "./routes/cardRoutes";
+import studySessionRoutes from "./routes/studySessionRoutes";
+import reviewAndProgressRoutes from "./routes/reviewAndProgressRoutes";
+import syncRoutes from "./routes/syncRoutes";
+import searchRoutes from "./routes/searchRoutes";
 
 const app = express();
 app.use(express.json());
@@ -12,22 +18,22 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
 // Decks
-app.use("/api/decks", userRoutes);
+app.use("/api/decks", deckRoutes);
 
 // Cards
-app.use("/api/cards", userRoutes);
+app.use("/api/cards", cardRoutes);
 
 //Study Sessions
-app.use("/api/study/session", userRoutes);
+app.use("/api/study/session", studySessionRoutes);
 
 // Review and Progress
-app.use("/api/review", userRoutes);
-app.use("/api/progress", userRoutes);
+app.use("/api/review", reviewAndProgressRoutes);
+app.use("/api/progress", reviewAndProgressRoutes);
 
 // Synchronization
-app.use("/api/sync", userRoutes);
+app.use("/api/sync", syncRoutes);
 
 // Search
-app.use("/api/search", userRoutes);
+app.use("/api/search", searchRoutes);
 
 export default app;
