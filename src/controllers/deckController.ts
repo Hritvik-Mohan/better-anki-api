5318824ab43a7d49da7ffbb2d3a0c19924839f04
Hritvik-Mohan <hritvik5005@gmail.com>
2025-01-13 00:00:17 +0530

    Update deck controller

--- src/controllers/deckController.ts
+++ src/controllers/deckController.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-export const getAllDecks = async (req: Request, res: Response): Promise<void> => {
+interface AuthenticateRequest extends Request {
+    user: { id: number }
+}
+
+export const getAllDecks = async (req: AuthenticateRequest, res: Response): Promise<void> => {
+    const userId = req.user?.id;
+
+    try {
+        const decks = await prisma.deck.findMany({
+            where: { userId },
+        });
+        
+        res.status(200).json(decks);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to fetch decks", error: (error as Error).message })
+    }
 }
 
 export const createDeck = async (req: Request, res: Response): Promise<void> => {
